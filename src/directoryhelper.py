ecc2a5404754178ea721b4d002b802063e82b5e7
Josh Parker <joshBBparker@gmail.com>
2024-06-23 13:35:32 -0600

    add the static to public copying function

---
+++ src/directoryhelper.py
@@ -0,0 +1,17 @@
+from os.path import exists, join, isfile
+from os import listdir, mkdir
+from shutil import rmtree, copy
+
+def make_copy(src, dst):
+    if exists(dst):
+        rmtree(dst)
+    mkdir(dst)
+    for f in listdir(src):
+        src_path = join(src, f)
+        dst_path = join(dst, f)
+        print(f'copying {src_path} to {dst_path}')
+        if isfile(src_path):
+            copy(src_path, dst_path)
+        else:
+            make_copy(src_path, dst_path)
+
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from textnode import TextNode
+from directoryhelper import make_copy
 
 def main():
  text_node = TextNode('This is a text node', 'bold', 'https://www.boot.dev')
  print(text_node)
+ make_copy('static', 'public')
 
 main()
