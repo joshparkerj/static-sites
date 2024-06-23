9526773694bc6178886df7563cc6bfed0fb37ee9
Josh Parker <joshBBparker@gmail.com>
2024-06-23 08:52:17 -0600

    add split image markdown and split text markdown methods

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,6 @@
-from enum import Enum
-
 from leafnode import LeafNode
+
+from texthelper import extract_markdown_images, extract_markdown_links
 
 class TextNode:
  def __init__(self, text, text_type, url=None):
@@ -41,5 +41,26 @@ class TextNode:
      if len(nodes) % 2 != 1:
          raise Exception('invalid Markdown syntax')
      return [TextNode(node, 'text' if i % 2 == 0 else text_type) for i, node in enumerate(nodes)]
-      
+
+ def split_image(self):
+     if self.text_type != 'text':
+         return [self]
+     markdown_images = extract_markdown_images(self.text)
+     nodes = [TextNode(self.text, 'text')]
+     for markdown_image in markdown_images:
+         node = nodes.pop()
+         texts = node.text.split(markdown_image['md'])
+         nodes += [TextNode(texts[0], 'text'), TextNode(markdown_image['alt'], 'image', markdown_image['src']), TextNode(texts[1], 'text')]
+     return nodes
+
+ def split_link(self):
+     if self.text_type != 'text':
+         return [self]
+     markdown_links = extract_markdown_links(self.text)
+     nodes = [TextNode(self.text, 'text')]
+     for markdown_link in markdown_links:
+         node = nodes.pop()
+         texts = node.text.split(markdown_link['md'])
+         nodes += [TextNode(texts[0], 'text'), TextNode(markdown_link['a_text'], 'link', markdown_link['href']), TextNode(texts[1], 'text')]
+     return nodes
 
