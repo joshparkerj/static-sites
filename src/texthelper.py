dbed99f53bb68814441a4fd2368e771c2b5f5a07
Josh Parker <joshBBparker@gmail.com>
2024-06-23 08:18:39 -0600

    add the md key to the extract function results

--- src/texthelper.py
+++ src/texthelper.py
@@ -3,12 +3,11 @@ from re import finditer
 def extract_markdown_images(text):
     alt_re = r'!\[(?P<alt>[^\]]*)\]'
     src_re = r'\((?P<src>[^\)]*)\)'
-    images = [{ 'alt': m.group('alt'), 'src': m.group('src') } for m in finditer(alt_re + src_re, text)]
+    images = [{ 'alt': m.group('alt'), 'src': m.group('src'), 'md': m.group(0) } for m in finditer(alt_re + src_re, text)]
     return images
 
 def extract_markdown_links(text):
-    a_text_re = r'[^!]\[(?P<a_text>[^\]]*)\]'
-    href_re = r'\((?P<href>[^\)]*)\)'
-    links = [{ 'a_text': m.group('a_text'), 'href': m.group('href') } for m in finditer(a_text_re + href_re, text)]
+    link_re = r'[^!](\[(?P<a_text>[^\]]*)\]\((?P<href>[^\)]*)\))'
+    links = [{ 'a_text': m.group('a_text'), 'href': m.group('href'), 'md': m.group(1) } for m in finditer(link_re, text)]
     return links
 
