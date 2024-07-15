0f96c375c327bba8fdb680208cf880d3bb9a5744
Josh Parker <joshBBparker@gmail.com>
2024-07-14 21:10:56 -0600

    copy paste one more test from https://tinyurl.com/bdff38u5

--- src/test_blockhelper.py
+++ src/test_blockhelper.py
@@ -169,6 +169,20 @@ This is the same paragraph on a new line
             ],
         )
 
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), "heading")
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), "code")
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), "quote")
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), "unordered_list")
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), "ordered_list")
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), "paragraph")
+
 
 if __name__ == "__main__":
     main()
