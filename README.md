d170ad62b18c22e72d5029b1dd34322cdc5d716b
Josh Parker <joshBBparker@gmail.com>
2024-07-14 18:43:48 -0600

    use pm2 to manage the server process

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # static sites
 
 from boot.dev
+
+I am using pm2 to manage the server process. see https://pm2.keymetrics.io/docs/usage/quick-start/
--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 cp static/index.css static/styles.css
 python3 src/main.py
-cd public && python3 -m http.server 8888
+cd public
+pm2 start --name ssg python3 -- -m http.server 8888
 
